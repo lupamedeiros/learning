964e70241f49978f47c81f14dbea13ab94d62d0c
Luiz Paulo <luizpaulo13@gmail.com>
2022-07-15 13:03:37 -0300

    learning_keras.py update

--- 02_tensor_flow/learning_keras.py
+++ 02_tensor_flow/learning_keras.py
@@ -26,3 +26,53 @@ linear_layer = Linear(4)
 y = linear_layer(tf.ones((2, 3)))
 
 assert len(linear_layer.weights) == 2
+
+class ComputeSum(Layer):
+    """Return the sum of two inputs."""
+
+    def __init__(self, input_dim):
+        super(ComputeSum, self).__init__()
+        # Create a num trainable weight
+        self.total = tf.Variable(initial_value=tf.zeros((input_dim,)),
+                                 trainable=False)
+
+    def call(self, inputs):
+        self.total.assign_add(tf.reduce_sum(inputs, axis=0))
+        return self.total
+
+
+my_sum = ComputeSum(2)
+x = tf.ones((2, 2))
+
+y = my_sum(x)
+print(y.numpy())
+
+y = my_sum(x)
+print(y.numpy())
+
+assert my_sum.weights == [my_sum.total]
+assert my_sum.non_trainable_weights == [my_sum.total]
+assert my_sum.trainable_weights == []
+
+
+class MLP(Layer):
+    """Simple stack of Linear layers."""
+
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.linear_1 = Linear(32)
+        self.linear_2 = Linear(32)
+        self.linear_3 = Linear(10)
+
+    def call(self, inputs):
+        x = self.linear_1(inputs)
+        x = tf.nn.relu(x)
+        x = self.linear_2(x)
+        x = tf.nn.relu(x)
+        return self.linear_3(x)
+
+mlp = MLP()
+
+y = mlp(tf.ones(shape=(3, 64)))
+
+assert len(mlp.weights) == 6
